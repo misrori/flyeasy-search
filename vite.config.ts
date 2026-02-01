722562dfc3e9541e9e3efe80c9b5800cf93a43bb
Mihály Orsós <ormraat.pte@gmail.com>
2026-02-01 16:02:07 +0100

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -5,7 +5,7 @@ import { componentTagger } from "lovable-tagger";
 
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => ({
-  base: "/flyeasy-search/",
+  base: "/",
   server: {
     host: "::",
     port: 8080,
